52dc72f99fcc44904e801a0088f6e6c098de3d27
Yomero3500 <110347908+Yomero3500@users.noreply.github.com>
2024-12-06 15:35:19 -0600

    click

--- escenas/Escena1.js
+++ escenas/Escena1.js
@@ -3,18 +3,23 @@ class Escena1 extends Phaser.Scene {
         super("bootGame");
     }
 
-preload(){
-    this.load.image("background", ".//assets/SCENARY/Battleground1.png");
-    this.load.image("plataforma", ".//assets/SCENARY/plataforma.png");
-    this.load.spritesheet("player", ".//assets/PLAYER/Idle.png", {frameWidth:128, frameHeight:128})
-    this.load.spritesheet("playerWalk", ".//assets/PLAYER/Walk.png", {frameWidth:128, frameHeight:128})
-    this.load.spritesheet("playerDead", ".//assets/PLAYER/Dead.png", {frameWidth:128, frameHeight:128})
-    this.load.spritesheet("playerRun", ".//assets/PLAYER/Run.png", {frameWidth:128, frameHeight:128})
-    this.load.image("enemy", ".//assets/ENEMIES/enemigo.png", {frameWidth:520, frameHeight:420})
-}
+    preload(){
+        this.load.image("background", ".//assets/SCENARY/Battleground1.png");
+        this.load.image("plataforma", ".//assets/SCENARY/plataforma.png");
+        this.load.spritesheet("player", ".//assets/PLAYER/Idle.png", {frameWidth:128, frameHeight:128});
+        this.load.spritesheet("playerWalk", ".//assets/PLAYER/Walk.png", {frameWidth:128, frameHeight:128});
+        this.load.spritesheet("playerDead", ".//assets/PLAYER/Dead.png", {frameWidth:128, frameHeight:128});
+        this.load.spritesheet("playerRun", ".//assets/PLAYER/Run.png", {frameWidth:128, frameHeight:128});
+        this.load.image("enemy", ".//assets/ENEMIES/enemigo.png", {frameWidth:520, frameHeight:420});
+    }
 
-create(){
-    this.add.text(20, 20, "Cargando juego...");
-    this.scene.start("playGame");
-}
+    create() {
+        // Mostrar texto para indicar que el juego está cargando
+        this.add.text(20, 20, "Cargando juego...");
+
+        // Esperar un clic del usuario para iniciar la siguiente escena
+        this.input.once('pointerdown', () => {
+            this.scene.start("playGame");
+        });
+    }
 }
